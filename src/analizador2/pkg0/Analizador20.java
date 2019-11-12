a25e5388b3d36b0e3ba142b3f92a13b754a5deb4
Sergio Cifuentes <sergiocifuentes1485@gmail.com>
2019-11-12 17:56:47 -0600

    Interfaz

---
+++ src/analizador2/pkg0/Analizador20.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analizador2.pkg0;
+
+import ui.Principal;
+
+/**
+ *
+ * @author sergio
+ */
+public class Analizador20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal principal = new Principal();
+        principal.setVisible(true);
+    }
+    
+}
